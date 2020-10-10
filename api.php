76a61e1b1c482b4af331118d3fe8f0b15e0e8f1a
Ph4x35 <46686861+AndreCCB@users.noreply.github.com>
2020-10-10 16:11:13 -0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,61 @@
+<?php
+//Usei esta lista de bandeiras
+//https://gist.github.com/erikhenrique/5931368
+$bandeira = array(
+						5=>"Mastercard",
+					    4=>"Visa",
+						301=>"Diners",
+						305=>"Diners",
+						36=>"Diners",
+						38=>"Diners",
+						636368=>"Elo",
+						636369=>"Elo",
+						438935=>"Elo",
+						504175=>"Elo",
+						451416=>"Elo",
+						636297=>"Elo",
+						5067=>"Elo",
+						4576=>"Elo",
+						4011=>"Elo",
+						506699=>"Elo",
+						34=>"Amex",
+						37=>"Amex",
+						6011=>"Discover",
+						622=>"Discover",
+						64=>"Discover",
+						65=>"Discover",
+						50=>"Aura",
+						35=>"Jcb",
+						38=>"Hipercard",
+						60=>"Hipercard"
+					);
+					$number = $bin;
+					//validator
+					$total = 0;
+					$i = 1;
+					$valid = $number;
+					$number = str_split($number);
+					$number = array_reverse($number);
+
+					foreach ($number as $digit) {
+						if($i % 2 == 0){
+							$digit *= 2;
+
+							if($digit > 9){
+								$digit -= 9;
+							}
+						}
+							$total += $digit;
+							$i++;
+						}
+						if($total % 10 == 0){
+											foreach ($bandeira as $key => $_bandeira) {
+							$tam = strlen($key);
+							$_bin = substr($bin, 0,$tam);
+							if($_bin==$key){
+								echo $bin.":".$_bandeira."<br>";
+							}
+						}
+		
+				    }
+?>
