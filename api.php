7a6762ea94e3cc8ee97c6c4d84aecbc05d1a4266
Ph4x35 <46686861+AndreCCB@users.noreply.github.com>
2020-10-13 15:55:52 -0300

    Update api.php

--- api.php
+++ api.php
@@ -4,32 +4,30 @@
 //https://www.4devs.com.br/gerador_de_numero_cartao_credito
 <?php
 $bandeira = array(
-	5=>"Mastercard",
-    	4=>"Visa",
-	301=>"Diners",
-	305=>"Diners",
-	36=>"Diners",
-	38=>"Diners",
 	636368=>"Elo",
 	636369=>"Elo",
-	438935=>"Elo",
 	504175=>"Elo",
+	506699=>"Elo",
+	438935=>"Elo",
 	451416=>"Elo",
 	636297=>"Elo",
 	5067=>"Elo",
 	4576=>"Elo",
 	4011=>"Elo",
-	506699=>"Elo",
 	34=>"Amex",
 	37=>"Amex",
 	6011=>"Discover",
 	622=>"Discover",
 	64=>"Discover",
 	65=>"Discover",
+	60=>"Hipercard",
 	50=>"Aura",
 	35=>"Jcb",
 	38=>"Hipercard",
-	60=>"Hipercard"
+	36=>"Diners",
+	38=>"Diners",
+	5=>"Mastercard",
+        4=>"Visa"
 );
 //bin = 123456278362345 
 $number = $bin;
